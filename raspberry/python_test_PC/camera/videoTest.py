5628ad790745fbdfb49c6332fa4fd5a018d8c4a4
angriff1 <dawmf@naver.com>
2021-04-27 16:45:17 +0900

    upload video

--- raspberry/python_test_PC/camera/videoTest.py
+++ raspberry/python_test_PC/camera/videoTest.py
@@ -4,7 +4,10 @@ import numpy as np
 from imageProcess import imageProcess, showTrackbar
 
 
-videoName = "videoTraff2.h264"
+videoName = "video427R.h264"
+saveName = "save427R.avi"
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+videocap = None
 
 
 if __name__ == "__main__":
@@ -23,11 +26,16 @@ if __name__ == "__main__":
             frame = buffer.tobytes()
 
             img_show, detected, i_param = imageProcess(img)
+            # if videocap is None:
+            #     videocap = cv2.VideoWriter(saveName, fourcc, 20.0, (640, 480))
+            # else:
+            #     videocap.write(img_show)
 
         k = cv2.waitKey(33) & 0xff
         if k == 27:  # press 'ESC' to quit
             break
         elif k == 64:
             time.sleep(10)
+    # videocap.release()
     video.release()
     cv2.destroyAllWindows()
